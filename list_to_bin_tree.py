5bce2e87df2949f0de1b10c90f375781d84f5759
Dylan Gallagher <gallagdy@tcd.ie>
2024-08-04 09:08:07 +0200

    Added list_to_bin_tree.py

---
+++ list_to_bin_tree.py
@@ -0,0 +1,48 @@
+import ast
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def list_to_bin_tree(node_list_str):
+    if node_list_str == "[]":
+        return None
+
+    node_val_list = ast.literal_eval(node_list_str.replace("null", "None"))
+
+    node_list = [None] * len(node_val_list)
+
+    for i, node_val in enumerate(node_val_list):
+        if isinstance(node_val, int):
+            node_list[i] = TreeNode(node_val)
+    
+    for i, node in enumerate(node_list):
+        if node:
+            left_child_index = 2*i + 1
+            right_child_index = 2*i + 2
+            
+            if left_child_index < len(node_list):
+                node.left = node_list[left_child_index]
+            if right_child_index < len(node_list):
+                node.right = node_list[right_child_index]
+
+    return node_list[0]
+
+if __name__ == "main":
+    # Example Usage
+    root = list_to_bin_tree("[3,9,20,null,null,15,7]")
+
+    def maxDepth(root):
+        def depth(node):
+            if not node:
+                return 0
+            return 1 + max(depth(node.left), depth(node.right))
+
+        node = root
+        return depth(node)
+
+    print(maxDepth(root))
